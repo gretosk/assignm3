149d16f3008b49993ed4ce6af58ba4e229978157
gretosk <101219020+gretosk@users.noreply.github.com>
2022-03-09 23:59:10 +0000

    verkefni4 update

--- bin/verkefni4.dart
+++ bin/verkefni4.dart
@@ -3,8 +3,11 @@ import 'dart:io';
 
 void main() {
 
+  // Náði ekki að klára verkefni með loop.
+
   print('Enter number');
   int num = int.tryParse(stdin.readLineSync(encoding: Encoding.getByName('UTF-8')));
+
 
   int length = num.toString().length;
 
